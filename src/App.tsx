13999feecc61e38f2b61086277b00e989bcc56fb
Andii <81762984+User6531@users.noreply.github.com>
2024-01-21 19:50:16 +0200

    update module patter button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Singleton from "./SingletonComponent";
 
-type TType = "singleton";
+type TType = "singleton" | "module";
 
 function App() {
   const [type, setType] = useState<TType>("singleton");
@@ -9,17 +9,28 @@ function App() {
   return (
     <div className="w-screen h-screen bg-gray-700 flex items-center justify-center text-white">
       <div className="flex flex-col">
-        <ul className="flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400">
+        <ul className="flex flex-wrap justify-center text-sm font-medium text-center text-gray-500 dark:text-gray-400">
           <li className="me-2">
             <button
-              className="inline-block p-4 text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500"
+              className={`inline-block p-4 ${
+                type === "singleton"
+                  ? "text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500"
+                  : "rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300"
+              }`}
               onClick={() => setType("singleton")}
             >
               Singleton
             </button>
           </li>
           <li className="me-2">
-            <button className="inline-block p-4 text-gray-400 rounded-t-lg cursor-not-allowed dark:text-gray-500">
+            <button
+              className={`inline-block p-4 ${
+                type === "module"
+                  ? "text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500"
+                  : "rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300"
+              }`}
+              onClick={() => setType("module")}
+            >
               Module
             </button>
           </li>
@@ -44,6 +55,9 @@ function getBody(type: TType) {
   switch (type) {
     case "singleton":
       return <Singleton />;
+
+    case "module":
+      return "module";
 
     default:
       return null;
